f95828ec7a57ae3132796b5d7be041f5f95500a0
maniprabhanshu <82037404+maniprabhanshu@users.noreply.github.com>
2021-12-25 10:31:15 +0530

    Add files via upload

--- study_dais/settings.py
+++ study_dais/settings.py
@@ -138,7 +138,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'quizes' / 'static',
 ]
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
