22fefd782b27db9e0aded0017737bfafd9217322
maniprabhanshu <82037404+maniprabhanshu@users.noreply.github.com>
2021-12-25 16:00:57 +0530

    Add files via upload

--- study_dais/settings.py
+++ study_dais/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-+xkf-c_%jdm26qb@$l5@yqz87+(0$ncnv+zp(p78r%xpk@l7^+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -166,7 +167,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # PWA_APP_LANG = 'en-US'
 #
 # PWA_SERVICE_WORKER_PATH=os.path.join(BASE_DIR,'static/js','serviceworker.js')
-
-
-
-# django_heroku.settings(locals())
